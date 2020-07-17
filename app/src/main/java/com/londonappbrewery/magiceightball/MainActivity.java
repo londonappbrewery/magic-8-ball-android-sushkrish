efb72cd1d5e1da6e06da674105e8880727504724
sushmitha krishnamoorthy <sushkrsh@gmail.com>
2020-07-16 21:49:19 -0400

    Added the 8ball and a button that randomly generates adifferent message.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int[] ball_img = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+        final ImageView eightballImg = findViewById(R.id.eightball_image);
+
+        Button ask = findViewById(R.id.ask_button);
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rngenerator = new Random();
+                int num = rngenerator.nextInt(5);
+                eightballImg.setImageResource(ball_img[num]);
+            }
+        });
     }
 }
